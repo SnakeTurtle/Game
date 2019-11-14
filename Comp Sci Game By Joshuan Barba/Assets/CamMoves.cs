b47bda3a1f09c586ddc1fc93572797ffcf1af344
SnakeTurtle <55854886+SnakeTurtle@users.noreply.github.com>
2019-11-14 15:21:29 -0500

    213

---
+++ Comp Sci Game By Joshuan Barba/Assets/CamMoves.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMoves : MonoBehaviour {
+
+    public GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 Cam = new Vector3(Player.transform.position.x + 5, 0, -10);
+        transform.position = Cam;
+
+    }
+
+}
---
+++ Comp Sci Game By Joshuan Barba/Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public Rigidbody2D rb2;
+    
+	// Use this for initialization
+	void Start () {
+        new Vector2(0, 250);
+        rb2 = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (Input.GetKeyDown("d"))
+        {
+            rb2.AddForce(new Vector3(500, 0, 0)); 
+        }
+
+        if (Input.GetKeyDown("w"))
+        {
+            rb2.AddForce(new Vector3(0, 500, 0));
+        }
+
+        if (Input.GetKeyDown("a"))
+        {
+            rb2.AddForce(new Vector3(-500, 0, 0));
+        }
+
+        if (Input.GetKeyDown("s"))
+        {
+            rb2.AddForce(new Vector3(0, -500, 0));
+        }
+
+
+
+
+    }
+}
